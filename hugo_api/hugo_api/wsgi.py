bcbc51ea9c3ac519a5d955c8d9e794a16af68c85
daniel9091 <djguzman1@miuandes.cl>
2024-11-22 13:58:45 -0300

    oo

--- hugo_api/hugo_api/wsgi.py
+++ hugo_api/hugo_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('hugo_api.settings', 'hugo_api.settings')
+os.environ.setdefault('hugo_api.hugo_api.settings', 'hugo_api.settings')
 
 application = get_wsgi_application()
